d7b22df5845151e3523cc868fb4a3e00d0c8dda1
Shariq Hashmi <127607558+bonkers23@users.noreply.github.com>
2025-02-17 21:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
-
+    const form = document.getElementById("form1");
+    const firstName = form.fname.value.trim();
+    const lastName = form.lname.value.trim();
+    alert(firstName + " " + lastName);
+    return false;
 }
